defaafe2b02becd8088143e317614d164e1aead2
Sydney Krantz <syd107@gmail.com>
2020-11-04 16:22:22 -0800

    express server and endpoints

--- frontend/server.js
+++ frontend/server.js
@@ -1,17 +1,46 @@
 const express = require("express");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-
 const app = express();
-const config = require("./webpack.config.js");
-const compiler = webpack(config);
+const port = 3000;
 
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
+// const webpack = require("webpack");
+// const webpackDevMiddleware = require("webpack-dev-middleware");
+// const config = require("./webpack.config.js");
+// const compiler = webpack(config);
 
-app.listen(3000, function () {
-  console.log("Listening on PORT 3000!");
+// app.use(
+//     webpackDevMiddleware(compiler, {
+//       publicPath: config.output.publicPath,
+//     })
+//   );
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.put("/user", function (req, res) {
+  res.send("Got a PUT request at /user");
+});
+
+app.post("/user", function (req, res) {
+  res.send("Got a POST request at /user");
+});
+
+app.delete("/user", function (req, res) {
+  res.send("Got a DELETE request at /user");
 });
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+// const autoprefixer = require("autoprefixer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
